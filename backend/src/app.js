4e3b1cb953ef9a9d4cdf2f03b6b024d7150b1261
Egor <zherelev92@mail.ru>
2024-09-24 16:28:05 +0300

    created analytics.controller

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,14 @@ const app = express();
 const sequelize = require('./config/db_connection.js');
 const articleRouter = require("./controllers/articles.js")
 const commentRouter = require("./controllers/comments.js")
+const analyticRouter = require("./controllers/analytics.js")
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/article", articleRouter);
 app.use("/comment", commentRouter);
+app.use("/analytic", analyticRouter);
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
---
+++ backend/src/controllers/analytics.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const { Op } = require('sequelize');
+const Comment = require('../models/comments');
+
+router.get('/comments/', async (req, res) => {
+  try {
+    const { dateFrom, dateTo } = req.query;
+    if (!dateFrom || !dateTo) {
+      const all_comments = await Comment.findAll();
+      res.json(all_comments);
+    }
+    else {
+      const comments = await Comment.findAll({
+        where: {
+          createdAt: {
+            [Op.between]: [dateFrom, dateTo]
+          }
+        },
+        group: ['ArticleId', 'id']
+      });
+
+      res.json(comments);
+    }
+  }
+  catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
